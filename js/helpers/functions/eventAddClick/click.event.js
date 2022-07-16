111031a81d199f1d2f99129236ccb52c15c1c3f7
abdeljail <taki22abdo@gmail.com>
2022-07-16 17:19:02 +0100

    add function for add event click in multiple elements and multiple function and update function clickElements :label:

--- js/helpers/functions/eventAddClick/click.event.js
+++ js/helpers/functions/eventAddClick/click.event.js
@@ -37,6 +37,11 @@ function clickElements({ elements, fun }) {
 
         if (!elements.length) throw new Error("required count in element > 0")
 
+
+        if (!typeof fun) throw new Error("required in type function ")
+
+        if (!fun.name) throw new Error("required in name function and function is found")
+
         elements.forEach((element, idx) => {
             if (!element.localName) throw new Error("required  tag name for elements =>  is not defined elment in index : " + idx)
             element.addEventListener("click", fun);
@@ -49,7 +54,52 @@ function clickElements({ elements, fun }) {
 }
 
 
+
+
+/**
+ *  this method is add event click in multiple elements and add same function
+ *  elements required element to listener Click
+ *  fun required function for excute on click 
+ */
+
+function clickElementsFunctions(array) {
+
+    try {
+
+
+
+        if (!(array instanceof Array)) throw "required in type array for elements"
+
+        if (!array.length) throw "required count in array > 0"
+
+        array.forEach((obj, idx) => {
+
+            if (!(obj instanceof Object)) throw "required in type object in array"
+
+            if (!Object.keys(obj).length) throw "required in  object in empty in index : " + idx
+
+            const [element, fun] = Object.values(obj)
+
+            if (!element.localName) throw "required  tag name for elements =>  is not defined elment in index : " + idx
+
+            if (!(typeof fun === "function")) throw new Error("required in type function in index : " + idx)
+            
+            if (!fun.name) throw new Error("required in name function and function is found")
+            
+            element.addEventListener("click", fun);
+        });
+
+    } catch (error) {
+        throw new Error(error)
+    }
+
+}
+
+
+
+
 export {
     clickElement,
-    clickElements
+    clickElements,
+    clickElementsFunctions
 }
