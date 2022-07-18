0e377459d199fa853e65732f23082f84d60e03ec
abdeljail <taki22abdo@gmail.com>
2022-07-18 11:32:13 +0100

    add methode for add event focus multiple element and excute same function :label:

--- js/helpers/functions/eventAddFocus/focus.event.js
+++ js/helpers/functions/eventAddFocus/focus.event.js
@@ -56,6 +56,43 @@ function focusElementFunctions({ element, functions }) {
 }
 
 
+/**
+ *  this method is add event focus in multiple elements and add same function
+ *  elements required element to listener Focus
+ *  fun required function for excute on Focus 
+ */
+
+function focusElements({ elements, fun }) {
+
+    try {
+
+        if (!(elements instanceof Array)) throw new Error("required in type array for elements")
+
+        if (!elements.length) throw new Error("required count in element > 0")
+
+
+        if (!typeof fun) throw new Error("required in type function ")
+
+        if (!fun.name) throw new Error("required in name function and function is found")
+
+        elements.forEach((element, idx) => {
+            if (!element.localName) throw new Error("required  tag name for elements =>  is not defined elment in index : " + idx)
+            element.addEventListener("focus", fun);
+        });
+
+    } catch (error) {
+        throw new Error(error)
+    }
+
+}
+
+
+
+
+
+
+
+
 
 
 
@@ -64,5 +101,6 @@ function focusElementFunctions({ element, functions }) {
 
 export {
     focusElement,
-    focusElementFunctions
+    focusElementFunctions,
+    focusElements
 }
